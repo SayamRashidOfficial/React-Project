a43cc9b7807eb491167d800cc3d6d6442e3f9b91
SayamRashidOfficial <pksayam111@gmail.com>
2024-06-28 21:22:10 +0500

    sample

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Resturent from './Component/basic/Resturent'
+// import UseState from './Component/Hooks/useState'
+// import UseEffect from './Component/Hooks/useEffect'
+// import UseReducer from './Component/Hooks/useReducer'
+// import List from "./Component/itemList/list"  
+// import Temp from './Component/weather/temp'
+function App() {
+  return (
+    <div>
+      {/* <Temp/> */}
+      {/* <List/> */}
+      {/* <UseReducer/> */}
+      {/* <UseEffect/> */}
+      {/* <UseState/> */}
+      <Resturent/>
+    </div>
+  )
+}
+
+export default App;
